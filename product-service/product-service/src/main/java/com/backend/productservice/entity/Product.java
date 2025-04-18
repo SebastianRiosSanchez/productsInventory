5df02baa17e15913a7b8fc78bdbfadbb60417706
SebastianRiosSanchez <sebastianrios47@gmail.com>
2025-04-18 17:14:47 -0500

    add attributes to Product class

--- product-service/product-service/src/main/java/com/backend/productservice/entity/Product.java
+++ product-service/product-service/src/main/java/com/backend/productservice/entity/Product.java
@@ -1,10 +1,15 @@
 package com.backend.productservice.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
 
 @Entity
-@Table(name = "product")
+@Table(name = "products")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,8 +19,31 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idProduct;
 
+    @Column(name = "product_name", nullable = false)
+    @Size(max = 255)
     private String productName;
 
+    @Column(name = "product_price", nullable = false)
     private Double productPrice;
+
+    @Column(name = "is_delete")
+    private Boolean isDelete;
+
+    @Column(name = "is_enable")
+    private Boolean isEnable;
+
+    @CreationTimestamp
+    @Column(name = "createdAt",
+            nullable = true,
+            updatable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updatedAt",
+            nullable = true,
+            updatable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Date updatedAt;
 
 }
